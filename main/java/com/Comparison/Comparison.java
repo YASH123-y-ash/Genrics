000068edeaeedc04772e790601e723297a69fe2c
Yash-y-ash <wardhan696@gmail.com>
2021-04-21 07:03:11 +0530

    Added: largest of three integer objects

---
+++ main/java/com/Comparison/Comparison.java
@@ -0,0 +1,27 @@
+package com.Comparison;
+
+public class Comparison {
+
+    public Comparison() {
+
+    }
+    // method to find  largest among three Integer
+    public static Integer largest(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if(y.compareTo(max) > 0) {
+            max = y;
+        }
+        if(z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        Integer xInt  = 41, yInt = 521, zInt = 68;
+        Integer max =  new Comparison().largest (xInt,yInt,zInt);
+        System.out.println("The largest is "+max);
+       
+    }
+}
---
+++ test/java/com/comparison/ComparisonTest.java
@@ -0,0 +1,18 @@
+package com.comparison;
+
+import com.Comparison.Comparison;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ComparisonTest {
+
+
+    @Test
+   // UC1-to validate given three integer value gives maximum return true
+    public void givenInteger_ShouldReturnLargest()
+    {
+        Comparison comparison = new Comparison();
+        int max = comparison.largest(45,24,98);
+        Assertions.assertEquals(98,max);
+    }
+}
